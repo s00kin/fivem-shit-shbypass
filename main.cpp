0d93f0847ff293b60ef7bcab2285cc0975091413
s00kin <66147422+s00kin@users.noreply.github.com>
2020-11-14 02:43:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
    while (true) // i dont like shit shbypass x) discord.gg/sokin
     {
-        *(BYTE*)(CustomAPI::GetModuleA("gta-core-five") + 0xDA898) = 1;
+        *(BYTE*)(CustomAPI::GetModuleA("gta-core-five") + 0xDA948) = 1;
         
     }
     return 0;
